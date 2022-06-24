5eb1fca95295384009e5f52e0c7cb1c69a5a4f52
LucaSk-x <lucapiccinno64@gmail.com>
2022-06-24 15:14:46 +0200

    second

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -2,7 +2,9 @@
     <div>
         <main>
             <div class="container">
-                
+                <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 mx-4">
+                    <div class="col p-3"></div>
+                </div>
             </div>
         </main>
     </div>
@@ -14,7 +16,31 @@
 
     export default {
         name: 'TheMain',
-    }
+
+        data() {
+            return {
+                apiURL: 'https://flynn.boolean.careers/exercises/api/array/music',
+                image: '',
+                text: '',
+                artist: '',
+                year: '',
+            }
+        },
+
+        methods: {
+            fetchSongList () {
+                axios.get(this.apiURL, {
+
+                })
+                .then((resp) => {
+                    this.text = resp.data.response;
+                    resp.response.poster;
+                    resp.response.poster;
+                    resp.response.poster;
+                });
+            }
+        },
+    };
 </script>
 
 <style lang="scss">
@@ -22,5 +48,8 @@
         height: 92vh;
         background-color: #1e2d3b ;
 
+        .col {
+            background-color: #2e3a46;
+        }
     }
 </style>
